6a30f686b1ee91c90a7ecd790e51a246e5047c7b
davidanew <davidanew@googlemail.com>
2019-03-11 09:56:40 +0000

    adding json function

--- main/main.go
+++ main/main.go
@@ -8,100 +8,105 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
-
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
 func main() {
+	const url = "https://search.roblox.com/catalog/json?SortType=RecentlyUpdated&IncludeNotForSale=false&Category=Collectibles&ResultsPerPage=30"
 
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("eu-west-1")},
 	)
-
 	if err != nil {
 		fmt.Println("Error creating session:")
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-
-	url := "https://search.roblox.com/catalog/json?SortType=RecentlyUpdated&IncludeNotForSale=false&Category=Collectibles&ResultsPerPage=1"
-
+	/*
 	response, err := myClient.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error in http request:")
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 	defer response.Body.Close()
-
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error in ioutil:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	dataJson := responseData
+	arr := JsonType{}
+	err = json.Unmarshal([]byte(dataJson), &arr.Array)
+	if err != nil {
+		fmt.Println("Error unmarshalling:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	*/
+
+	arr, err := getJson(url , 1)
+	if err != nil {
+		fmt.Println("In getting JSON:")
+		fmt.Println(err.Error())
 	}
 
 
-	dataJson := responseData
-	arr := JsonType{}
-	_ = json.Unmarshal([]byte(dataJson), &arr.Array)
-	//TODO: put in error checking
-	fmt.Printf("Name is %d \n",arr.Array[0].Name)
-	fmt.Printf("AssetId is %s \n",arr.Array[0].AssetId)
-	fmt.Printf("Updated is %s \n",arr.Array[0].Updated)
+	//println(arr)
 
-	// Create DynamoDB client
 	svc := dynamodb.New(sess)
-
-//	assetId := arr.Array[0].AssetId
-//	name := arr.Array[0].Name
-
-
-
-
-//	items := getItems()
-
-	// Add each item to Movies table:
-//	for _, item := range items {
-		av, err := dynamodbattribute.MarshalMap(arr.Array[0])
-
+	for _, item := range arr.Array {
+		fmt.Printf("Processing %s \n", item.Name)
+		av, err := dynamodbattribute.MarshalMap(item)
 		if err != nil {
 			fmt.Println("Got error marshalling map:")
 			fmt.Println(err.Error())
 			os.Exit(1)
 		}
-
-		// Create item in table Movies
 		input := &dynamodb.PutItemInput{
-			Item: av,
+			Item:      av,
 			TableName: aws.String("RobloxCollectibles"),
 		}
-
 		_, err = svc.PutItem(input)
-
 		if err != nil {
 			fmt.Println("Got error calling PutItem:")
 			fmt.Println(err.Error())
 			os.Exit(1)
 		}
-
-		//fmt.Println("Successfully added '",item.Title,"' (",item.Year,") to Movies table")
-//	}
+	}
 }
 
 type JsonType struct {
 	Array []struct{
 		AssetId		   int64
 		Name           string
-		Updated        string
 	}
 }
 
-
-
-
-
+func getJson(urlBase string, pageNumber  int) (JsonType , error) {
+	var url  = fmt.Sprintf("%s&PageNumber=%d", urlBase, pageNumber)
+	response, err := myClient.Get(url)
+	if err != nil {
+		return JsonType{}, fmt.Errorf("Error in http request: %s", err.Error())
+	}
+	defer response.Body.Close()
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return JsonType{}, fmt.Errorf("Error in ioutil: %s", err.Error())
+	}
+	dataJson := responseData
+	arr := JsonType{}
+	err = json.Unmarshal([]byte(dataJson), &arr.Array)
+	if err != nil {
+		return JsonType{}, fmt.Errorf("Error in unmarshalling: %s", err.Error())
+	}
+	return arr , nil
+}
 
 
 
